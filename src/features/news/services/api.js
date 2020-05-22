786232fc8507927359b05ba8c64ba7510a0d7e30
Sergey <kulesh.sp@gmail.com>
2020-05-22 15:13:16 +1000

    Fixes CORS

--- src/features/news/services/api.js
+++ src/features/news/services/api.js
@@ -1,7 +1,9 @@
 export default {
   getNews: (pageSize, page) => {
-    return fetch(`https://newsapi.org/v2/top-headlines?country=us&category=technology&pageSize=${pageSize}&page=${page}&apiKey=abc92bff58e14fc39700e8ac5ec900a1`).then(response =>
-      response.json()
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    const targetUrl = `https://newsapi.org/v2/top-headlines?country=us&category=technology&pageSize=${pageSize}&page=${page}&apiKey=abc92bff58e14fc39700e8ac5ec900a1`
+    return fetch(proxyUrl + targetUrl).then(blob => blob.json()).then(response =>
+      response
     );
   }
 };
